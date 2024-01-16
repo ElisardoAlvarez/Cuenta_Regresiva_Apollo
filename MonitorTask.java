07d4f6849b3f1eb3af0bd344be82ae13c225137b
elisardo <elisalma@myuax.com>
2024-01-16 20:17:38 +0100

    version 2.0.2

---
+++ MonitorTask.java
@@ -0,0 +1,33 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MonitorTask implements Runnable {
+    private final CountdownTask countdownTask;
+    private final Logger logger;
+    private CountdownTask.State lastState;
+
+    public MonitorTask(CountdownTask countdownTask, Logger logger) {
+        this.countdownTask = countdownTask;
+        this.logger = logger;
+        this.lastState = null;
+    }
+
+    @Override
+    public void run() {
+        while (countdownTask.isAlive()) {
+            CountdownTask.State currentState = countdownTask.getCurrentState();
+            if (currentState != lastState) {
+                logger.log(Level.INFO, "Countdown Task State Changed: " + currentState);
+                lastState = currentState;
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                logger.log(Level.SEVERE, "Monitor thread interrupted", e);
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+    }
+}
